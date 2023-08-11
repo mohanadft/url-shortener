29af79823247b6d5b86ff5293ea768bd3a04a888
mohanadft <mohanadfteha@gmail.com>
2023-08-11 22:42:06 +0300

    :sparkles: Add `addUrl` method
    to add a url based on the input

--- app/server/src/services/url.service.ts
+++ app/server/src/services/url.service.ts
@@ -8,3 +8,7 @@ export const getUrls = async (): Promise<Url[]> => {
 export const getUrl = async (id: string): Promise<Url | null> => {
   return await urlModel.findUnique({ where: { id } });
 };
+
+export const addUrl = async (data: Prisma.UrlCreateInput): Promise<Url> => {
+  return await urlModel.create({ data });
+};
