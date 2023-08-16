e8c38586264264366883df3d292e4105d6f06530
mohanadft <mohanadfteha@gmail.com>
2023-08-16 23:22:54 +0300

    :sparkles: Invoke the `addUrl` method to the desired route

--- app/server/src/routers/url.router.ts
+++ app/server/src/routers/url.router.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { PrismaClient } from '@prisma/client';
 import { UrlService } from '../services/url.service';
 import { UrlController } from '../controllers/url.controller';
+import { use } from '../helpers';
 
 const prisma = new PrismaClient();
 const urlService = new UrlService(prisma);
@@ -11,5 +12,6 @@ const urlRouter = Router();
 
 urlRouter.get('/', urlController.getUrls.bind(urlController));
 urlRouter.get('/:id', urlController.getUrl.bind(urlController));
+urlRouter.post('/', urlController.addUrl.bind(urlController));
 
 export default urlRouter;
